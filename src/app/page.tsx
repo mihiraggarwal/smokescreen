7fe1f7e67d52d60f306f7801dd4df2b6236e21f5
Mihir Aggarwal <mihir.aggarwal1000@gmail.com>
2025-06-28 16:01:27 +0530

    calendar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,12 @@ import Papa from 'papaparse'
 import { toast } from 'react-hot-toast'
 import 'dotenv/config'
 import AppsIcon from '@mui/icons-material/Apps';
+import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
+import { DateCalendar } from '@mui/x-date-pickers/DateCalendar';
+import dayjs, { Dayjs } from 'dayjs';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import Modal from '@mui/material/Modal';
 
 import Dashboard from './_components/dash'
 
@@ -33,6 +39,10 @@ export default function Home() {
   const [windSpeed, setWindSpeed] = useState<number>(0)
   const [windDirection, setWindDirection] = useState<string>('')
   const [dashboardVisible, setDashboardVisible] = useState(true)
+  const [allPoints, setAllPoints] = useState<any[]>([])
+  const [calendarPoints, setCalendarPoints] = useState<any[]>([])
+  const [calendarVisible, setCalendarVisible] = useState(false)
+  const [selectedDate, setSelectedDate] = useState<Dayjs | null>(dayjs());
 
   type csv = {
     latitude: string;
@@ -97,6 +107,23 @@ export default function Home() {
               return row.confidence && ['nominal', 'high'].includes(row.confidence.toLowerCase())
             })
             setFirePoints(filtered)
+          }
+        })
+      })
+  }, [])
+
+  useEffect(() => {
+    fetch('/api/viirs/all')
+      .then(res => res.text())
+      .then(csvText => {
+        Papa.parse(csvText, {
+          header: true,
+          complete: (results) => {
+            const data = results.data as csv[];
+            const filtered = data.filter((row) => {
+              return row.confidence && ['nominal', 'high'].includes(row.confidence.toLowerCase())
+            })
+            setAllPoints(filtered);
           }
         })
       })
@@ -182,26 +209,64 @@ export default function Home() {
     return Math.min(25, Math.max(3.5, frp / 5))
   }
 
+  const filterCalendarPoints = (date: Dayjs) => {
+    const formattedDate = date.format('YYYY-MM-DD');
+    const filteredPoints = allPoints.filter(point => point.acq_date == formattedDate);
+    setCalendarPoints(filteredPoints);
+  }
+
+  function Calendar() {
+    return (
+      <Modal open={calendarVisible} onClose={() => setCalendarVisible(false)}>
+        <div className="absolute block mx-auto top-1/2 right-1/2 -translate-y-1/2 translate-x-1/2 z-[1000] bg-white p-4 rounded shadow-md text-black">
+          <div className='mb-2 mt-2 flex flex-row justify-end'>
+            <button
+              className="ml-auto mr-6 px-4 py-1 text-sm rounded-full bg-gray-100 hover:bg-gray-200 text-gray-700 shadow-sm border border-gray-300 transition-all hover:cursor-pointer" 
+              onClick={() => {
+                setSelectedDate(dayjs());
+                setCalendarVisible(false);
+                setCalendarPoints([])
+              }}
+            >
+                Today
+            </button>
+          </div>
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <DateCalendar minDate={dayjs().subtract(7, 'day')} maxDate={dayjs()} value={selectedDate} onChange={((val) => {
+              setSelectedDate(val);
+              filterCalendarPoints(dayjs(val))
+            })} />
+          </LocalizationProvider>
+        </div>
+      </Modal>
+    )
+  }
+
   return (
     <div className="relative h-screen w-screen">
-      <div className="absolute top-4 right-4 z-[1000] flex flex-col gap-2 bg-white p-2 rounded shadow-md">
-        {/* <button
-          className='bg-blue-500 text-white px-4 py-2 rounded hover:cursor-pointer'
-          onClick={() => {setIndex(0)}}
+      <div className="absolute top-6 right-6 z-[1000] flex flex-col gap-4 rounded-full">
+        <button
+          className='bg-black text-white px-2.5 py-2.5 rounded-full hover:cursor-pointer'
+          onClick={() => {}}
         >
-          🔥
+          <CalendarMonthIcon />
         </button>
         <button
-          className='bg-blue-500 text-white px-4 py-2 rounded hover:cursor-pointer'
-          onClick={() => {setIndex(1)}}
+          className='bg-black text-white px-2.5 py-2.5 rounded-full hover:cursor-pointer'
+          onClick={() => {
+            setCalendarVisible(!calendarVisible);
+          }}
         >
-          🔥
-        </button> */}
+          <CalendarMonthIcon />
+        </button>
       </div>
+      {calendarVisible && (
+          <Calendar />
+      )}
       {(index === 1) && (
         <div className="absolute bottom-6 right-6 z-[1000] flex flex-col gap-2 bg-white rounded-full shadow-md">
           <button
-            className='bg-blue-500 text-white rounded-full px-2.5 py-2.5 hover:cursor-pointer'
+            className='bg-black text-white rounded-full px-2.5 py-2.5 hover:cursor-pointer'
             onClick={() => {setDashboardVisible(!dashboardVisible)}}
           >
             <AppsIcon />
@@ -224,6 +289,8 @@ export default function Home() {
               setCenter(pinLs[parseFloat(pin)])
               setZoom(12);
               setIndex(1);
+              setSelectedDate(dayjs());
+              setCalendarPoints([]);
             }
             else {
               setIndex(0);
@@ -239,18 +306,40 @@ export default function Home() {
             <TileLayer
               url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
-            {firePoints.map((point: any, idx: number) => (
-              <CircleMarker
-                key={idx}
-                center={[parseFloat(point.latitude), parseFloat(point.longitude)]}
-                radius={computeRadius(point.frp)}
-                pathOptions={{ stroke: false, fillColor: 'red', fillOpacity: 0.4 }}
-              >
-                <Tooltip>
-                  <span className='text-sm'>Date: {point.acq_date}<br />Time: {String(Math.floor(point.acq_time / 100)).padStart(2, '0')}:{String(point.acq_time % 100).padStart(2, '0')}<br />Fire Radiative Power: {point.frp || 'N/A'}</span>
-                </Tooltip>
-              </CircleMarker>
-            ))}
+            {calendarPoints.length == 0 && (
+              <>
+                {firePoints.map((point: any, idx: number) => (
+                  <CircleMarker
+                    key={idx}
+                    center={[parseFloat(point.latitude), parseFloat(point.longitude)]}
+                    radius={computeRadius(point.frp)}
+                    pathOptions={{ stroke: false, fillColor: 'red', fillOpacity: 0.4 }}
+                  >
+                    <Tooltip>
+                      <span className='text-sm'>Date: {point.acq_date}<br />Time: {String(Math.floor(point.acq_time / 100)).padStart(2, '0')}:{String(point.acq_time % 100).padStart(2, '0')}<br />Fire Radiative Power: {point.frp || 'N/A'}</span>
+                    </Tooltip>
+                  </CircleMarker>
+                ))}
+              </>
+            )}
+
+            {calendarPoints.length > 0 && (
+              <>
+                {calendarPoints.map((point: any, idx: number) => (
+                  <CircleMarker
+                    key={idx}
+                    center={[parseFloat(point.latitude), parseFloat(point.longitude)]}
+                    radius={computeRadius(point.frp)}
+                    pathOptions={{ stroke: false, fillColor: 'red', fillOpacity: 0.4 }}
+                  >
+                    <Tooltip>
+                      <span className='text-sm'>Date: {point.acq_date}<br />Time: {String(Math.floor(point.acq_time / 100)).padStart(2, '0')}:{String(point.acq_time % 100).padStart(2, '0')}<br />Fire Radiative Power: {point.frp || 'N/A'}</span>
+                    </Tooltip>
+                  </CircleMarker>
+                ))}
+              </>
+            )}
+
           </MapContainer>
           
         </>
